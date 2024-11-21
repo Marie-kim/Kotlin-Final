b53c0d03132668dd6307dd30da3eb86f8adeec57
marie-kim <nyambukim90@gmail.com>
2024-11-21 21:58:05 +0300

    initial commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('service/', views.service, name='service'),
     path('starter/', views.starter, name='starter'),
+    path('about/', views.about, name='about'),
+    path('doctors/', views.doctors, name='doctors'),
+    path('services/', views.services, name='services'),
+    path('appointment/', views.appointment, name='appointment'),
+    path('contact/', views.contact, name='contact'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from myapp.models import Appointment
+from myapp.models import Contact
 
 # Create your views here.
 def index(request):
@@ -9,3 +11,41 @@ def service(request):
 
 def starter(request):
     return render(request,'starter-page.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def doctors(request):
+    return render(request,'doctors.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def appointment(request):
+    if request.method == 'POST':
+        myappointment=Appointment(
+            name = request.POST['name'],
+            email = request.POST['email'],
+            phone = request.POST('phone'),
+            datetime = request.POST('date'),
+            department = request.POST('department'),
+            doctor = request.POST('doctor'),
+            message = request.POST('message'),
+        )
+        myappointment.save()
+        return redirect('/appointment')
+    else:
+        return render(request, 'appointment.html')
+
+def contact(request):
+    if request.method == 'POST':
+        mycontact=Contact(
+            name = request.POST['name'],
+            email = request.POST['email'],
+            subject = request.POST('subject'),
+            message = request.POST('message'),
+        )
+        contact.save()
+        return redirect('/contact')
+    else:
+        return render(request, 'contact.html')
